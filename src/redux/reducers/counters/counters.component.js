4a5abcccd5ef42d5b290e4b19b1471f6cf127b45
wil.i.am <wiliam.tae@gmail.com>
2020-01-05 11:53:47 -0300

    initial state and actions passed as variables

--- src/redux/reducers/counters/counters.component.js
+++ src/redux/reducers/counters/counters.component.js
@@ -1,14 +1,24 @@
-const counters = (state, action) => {
+export const ADD_COUNTER = 'ADD_COUNTER';
+export const REMOVE_COUNTER = 'REMOVE_COUNTER';
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+const INITIAL_STATE = []
+
+const counters = (state = INITIAL_STATE, action) => {
   switch(action.type) {
-    case 'ADD_COUNTER':
+    case ADD_COUNTER:
       return state.concat(0);
-    case 'REMOVE_COUNTER':
+    case REMOVE_COUNTER:
       return state.filter((_, index) => index !== action.index);
-    case 'INCREMENT':
+    case INCREMENT:
       return state.map((elem, ind) => ind === action.index ? elem + 1 : elem)
-    case 'DECREMENT':
+    case DECREMENT:
       return state.map((elem, ind) => ind === action.index ? elem - 1 : elem)
+    default:
+      return state;
   }
+
 }
 
 export default counters;
--- src/redux/reducers/counters/counters.test.js
+++ src/redux/reducers/counters/counters.test.js
@@ -1,4 +1,4 @@
-import counters from './counters.component';
+import counters, { ADD_COUNTER, REMOVE_COUNTER, INCREMENT, DECREMENT } from './counters.component';
 import { expect } from 'chai';
 import deepFreeze from 'deep-freeze';
 
@@ -8,7 +8,7 @@ it('counters should be a function', () => {
 
 it('should add a counter', () => {
   const before = deepFreeze([]);
-  const action = deepFreeze({ type: 'ADD_COUNTER' });
+  const action = deepFreeze({ type: ADD_COUNTER });
   const after = [0]
   expect(counters(before, action)).to.be.deep.equal(after);
 })
@@ -16,49 +16,64 @@ it('should add a counter', () => {
 
 it('should add a counter', () => {
   const before = deepFreeze([0 ,1]);
-  const action = deepFreeze({ type: 'ADD_COUNTER' });
+  const action = deepFreeze({ type: ADD_COUNTER });
   const after = [0, 1, 0];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
 
 it('should remove a counter', () => {
   const before = deepFreeze([0, 1, 0]);
-  const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 1 });
+  const action = deepFreeze({ type: REMOVE_COUNTER, index: 1 });
   const after = [0, 0];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
 
 it('should remove a counter', () => {
   const before = deepFreeze([0, 1, 0]);
-  const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 2 });
+  const action = deepFreeze({ type: REMOVE_COUNTER, index: 2 });
   const after = [0, 1];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
 
 it('should increment a counter', () => {
   const before = deepFreeze([0, 0]);
-  const action = deepFreeze({ type: 'INCREMENT', index: 1 });
+  const action = deepFreeze({ type: INCREMENT, index: 1 });
   const after = [0, 1];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
 
 it('should increment a counter', () => {
   const before = deepFreeze([0, 0]);
-  const action = deepFreeze({ type: 'INCREMENT', index: 0 });
+  const action = deepFreeze({ type: INCREMENT, index: 0 });
   const after = [1, 0];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
 
 it('should decrement a counter', () => {
   const before = deepFreeze([0, 0]);
-  const action = deepFreeze({ type: 'DECREMENT', index: 0 });
+  const action = deepFreeze({ type: DECREMENT, index: 0 });
   const after = [-1, 0];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
 
 it('should decrement a counter', () => {
   const before = deepFreeze([0, 0]);
-  const action = deepFreeze({ type: 'DECREMENT', index: 1 });
+  const action = deepFreeze({ type: DECREMENT, index: 1 });
   const after = [0, -1];
+  expect(counters(before, action)).to.be.deep.equal(after);
+})
+
+
+it('should return the before state if the action is undefined', () => {
+  const before = deepFreeze([0, 0, 2]);
+  const action = deepFreeze({ type: 'UNKNOWN'});
+  const after = [0, 0, 2];
+  expect(counters(before, action)).to.be.deep.equal(after);
+})
+
+it('should return the initial state if before is undefined', () => {
+  const before = undefined;
+  const action = deepFreeze({});
+  const after = [];
   expect(counters(before, action)).to.be.deep.equal(after);
 })
